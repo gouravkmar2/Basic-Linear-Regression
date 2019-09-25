6cbe63deeaa9ea444f891baf45e3ec544b881b20
dinesh196 <chavandinesh196@gmail.com>
2019-09-25 12:34:42 +0530

    Completed image classifier

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+  # if i % 2000 == 1999:    # print every 2000 mini-batches
+        #     print('[%d, %5d] loss: %.3f' %
+        #           (epoch + 1, i + 1, running_loss / 2000))
+        #     running_loss = 0.0
